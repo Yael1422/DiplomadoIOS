4e3a02265a8291b0778591ec6063acb84df34a5f
Gandhi Yael Puente López <yael.puentelpz@gmail.com>
2024-01-04 22:58:25 -0600

    Ejercicio extra modulo 5

---
+++ Módulo 5/SwiftUI/SwiftUIBasics/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIBasics
+//
+//  Created by Diplomado on 08/12/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        HStack {
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
